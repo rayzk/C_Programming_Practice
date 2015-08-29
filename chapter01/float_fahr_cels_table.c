d9170f77f6ccc315ebaa992707a6a3e4eb8bcd02
ZK <zhang2121619kai@gmail.com>
2015-08-29 16:02:05 +0800

    Chapter01 codes

--- chapter01/float_fahr_cels_table.c
+++ chapter01/float_fahr_cels_table.c
@@ -6,10 +6,10 @@
 
 
 main(){
-
+	printf ("Table of Fahr and cels:\n");
 	int fahr;
 
-	for (fahr = 0; fahr <= 300; fahr += 20){
+	for (fahr = LOWER; fahr <= UPPER; fahr += STEP){
 
 		// only one of 5.0/9.0 need to be float number, the result will be float number.
 		printf("%3d\t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
