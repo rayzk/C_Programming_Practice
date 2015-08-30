e1567cbe29136a2adfccecdb757fea3dacb52029
ZK <zhang2121619kai@gmail.com>
2015-08-30 08:45:33 +0800

    File Comparator
    only compare two files at char level

--- chapter01/txt_comparator/file_char_comparator.c
+++ chapter01/txt_comparator/file_char_comparator.c
@@ -2,7 +2,7 @@
 
 int main(){
 
-	FILE *file1, *file2;
+	FILE *file_pointer1, *file_pointer2;
 	int char1, char2;
 	char file_name1[40], file_name2[40];
 
@@ -16,10 +16,10 @@ int main(){
 
 	// test the validity of the file.
 	if (file_pointer1 == NULL){
-		printf ("The first file does not exit.");
+		printf ("The first file %s does not exit.", file_name1);
 		exit(1);
 	}else if (file_pointer2 == NULL){
-		printf ("The second file does not exit.");
+		printf ("The second file %s does not exit.", file_name2);
 		exit(1);
 	}
 
@@ -27,6 +27,19 @@ int main(){
 	char1 = getc(file_pointer1);
 	char2 = getc(file_pointer2);
 
-	while
+	while((char1 != EOF) && (char2 != EOF) && (char1 == char2)){
+		char1 = getc(file_pointer1);
+		char2 = getc(file_pointer2);
+	}
 
+	if (char1 == char2){
+		printf ("Files are identical.\n");
+	}else {
+		printf ("Files are not identical\n");
+	}
+
+	fclose(file_pointer1);
+	fclose(file_pointer2);
+
+	return (0);
 }
